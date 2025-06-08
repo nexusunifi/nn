3cc00043599cbd26dc85bee11fbbe85d7c38c1b6
nexusunifi <nishusoni1011@gmail.com>
2025-06-09 01:17:02 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>SASU Expense Tracker</h1>
+      <p>This is a fresh deployment of your app. Ready to expand!</p>
+    </div>
+  );
+}
+
+export default App;
